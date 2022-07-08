3a28284cbd7e43e9221d74d38dbdb653d68679a7
Andres Dalmau <73449760+axdny@users.noreply.github.com>
2022-07-08 20:08:54 -0300

    mejorando desafio

--- 01_desafio/main.js
+++ 01_desafio/main.js
@@ -1,8 +1,9 @@
 let frase = '';
+let cantidad = parseInt(prompt('Ingresa la cantidad de palabras que quieres que tenga tu frase'));
 
-for (i = 0; i < 5; i++){
-  frase += prompt('Ingresa una palabra para formar una frase.\nSon 5 palabras en total.') + " ";
+for (i = 0; i < cantidad; i++){
+  frase += prompt(`Ingresa una palabra.\nSon ${cantidad} palabras en total.`) + " ";
 }
 
-document.write(`<h2 style=color{red};>${frase}</h2>`);
+document.write(`<h2>${frase}</h2>`);
 console.log(`Con las palabras ingresadas formamos: ${frase}`);
