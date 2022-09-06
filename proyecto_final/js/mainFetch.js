337360ccfd0cb2d74d48fa8ee9d5ebe213aa65a4
Andres Dalmau <73449760+axdny@users.noreply.github.com>
2022-09-06 19:54:21 -0300

    test

--- proyecto_final/js/mainFetch.js
+++ proyecto_final/js/mainFetch.js
@@ -35,7 +35,7 @@ const addBook = () => {
 //Función que muestra todos los libros que tenemos guardados en la biblioteca.
 const showLibrary = () => {
   $main.innerHTML = LibraryUI;
-  fetch('https://my-json-server.typicode.com/axdny/javascript_coderhouse/')
+  fetch('https://my-json-server.typicode.com/axdny/javascript_coderhouse/books')
     .then(response => response.json())
     .then(json => {
       json.forEach(el => {
